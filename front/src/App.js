e0dad36874de047f7350abe8fde26a00c1b90544
jpinto10 <jpinto10@gmail.com>
2023-01-15 12:41:04 -0300

    ajustado a criação inicial da pagina principal

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,13 @@
 import './Global.css';
 import Login from './pages/Login';
 
+import Principal from './pages/Principal';
+
+
 function App() {
   return (
-      <Login />
+      // <Login />
+      <Principal/>
   );
 }
 
--- front/src/pages/Principal/index.jsx
+++ front/src/pages/Principal/index.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import './index.css';
+import Logo from '../../assets/CMS.png';
+
 
 const Principal = (props) => {
     return (
-        <div className='principal-container'>
-            <div className='cabec'>
-                visitante
+            <div className='principal-container'>
+                <div className='cabec'>
+                    cabec
+                </div>
+                
+                <div className='body'>
+                    <img src={Logo} alt='Logo da empresa'/>
+                    {props.children}
+                </div>
+                
+                <div className='rodape'>
+                    rodape
+                </div>
             </div>
-            <img src=''/>
-            
-            <div className='rodape'>
-                visitante
-            </div>
-        </div>
       );
 }
 
