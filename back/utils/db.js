e4567439ea4f810f9360de2cdf2f598fa6191720
jpinto10 <jpinto10@gmail.com>
2023-01-24 10:50:14 -0300

    ajuste para manter a conexão de apenas um banco SGP em sql

--- back/utils/db.js
+++ back/utils/db.js
@@ -1,30 +1,7 @@
-// const mssql = require('mssql') 
-// require('dotenv').config();
-// //const connStr = `Server=${process.env.SERV};Database=${process.env.BANCO};User Id=${process.env.USUARIO};Password=${process.env.SENHA};TrustServerCertificate=true`;
-// const connStr = "Server=172.30.0.212;Database=MAPA;User Id=sa;Password=dese@123;encrypt=false;";
-
-// module.exports = mssql.connect(connStr)
-//    .then(conn => {
-//       global.conn = conn
-//       console.log("conectou!")} )
-//    .catch(err => console.log("erro! " + err));
-
-
-// module.exports = {
-//    execSqlquery(sqlQuery, res){
-//       console.log("url " + sqlQuery)
-//       global.conn.request()
-//                .query(sqlQuery)
-//                .then( resultQuery => res.json(resultQuery.recordset))
-//                .catch(errSql => res.json(errSql))
-//    }
-// }
 const sql = require('mssql')
-//empresa
-// const config = "Server=172.30.0.112;Database=MAPA;User Id=sa;Password=f431T@s@#2022;encrypt=false;";
 
 //CASA
-const config = "Server=localhost;Database=SGP;User Id=sa;Password=sa;encrypt=false;";
+const config = "Server=192.168.100.71;Database=SGP;User Id=sa;Password=sa;encrypt=false";
 
 // async/await style:
 const pool1 = new sql.ConnectionPool(config);
